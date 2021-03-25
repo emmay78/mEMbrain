8e9f7c684aacdd34ed7b056851293a88939a87b8
Emma Yang <emmay78@users.noreply.github.com>
2021-03-25 17:22:45 -0400

    Laying groundwork for full patch generation preprocessing in GUI and preprocessing function

---
+++ patchGenerationPreprocessing.m
@@ -0,0 +1,11 @@
+function [] = patchGenerationPreprocessing(inputImage,labelImage, patchDensity, patchSize)
+%PATCHGENERATIONPREPROCESSING preprocesses input and label images before patch generation
+%   Images are preprocessed, including resizing, contrast, and coloring,
+%   per user specifications. They are then passed to the [TO BE FILLED] for
+%   patch generation.
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
+
