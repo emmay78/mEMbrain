34ce63404c937f5a4aa024ed1cdd518548019cb3
Emma Yang <emmay78@users.noreply.github.com>
2021-03-26 15:55:44 -0400

    Completed patch generation preprocessing function and beginning patchGeneration function

--- patchGenerationPreprocessing.m
+++ patchGenerationPreprocessing.m
@@ -1,11 +1,37 @@
-function [] = patchGenerationPreprocessing(inputImage,labelImage, patchDensity, patchSize)
-%PATCHGENERATIONPREPROCESSING preprocesses input and label images before patch generation
-%   Images are preprocessed, including resizing, contrast, and coloring,
-%   per user specifications. They are then passed to the [TO BE FILLED] for
-%   patch generation.
+function [] = patchGenerationPreprocessing(inputImage, labelImage, inputPatchDirectory, labelPatchDirectory, numberOfClasses, patchDensity, patchSize, contrastCorrect, downsamplingFactor)
+%PATCHGENERATIONPREPROCESSING Preprocesses input and label images before patch generation
+%   Images are preprocessed, including resizing and coloring
+%   per user specifications. They are then passed to the generatePatches 
+%   function for patch generation.
 
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+% If requested by user, downsample the input image. If downsamplingFactor
+% is not specified to 1 so imresize has no effect.
+
+if ~exist('downsamplingFactor', 'var')
+    downsamplingFactor = 1;
+end
+
+inputImage_resized = imresize(inputImage, 1/downsamplingFactor);
+
+% If requested by user, the contrast of the input image is correct either
+% by patch or for the entire image using CLAHE in the contrastCorrect
+% function
+
+if exist('contrastCorrect', 'var')
+    if contrastCorrect == "patch"
+        inputImage_corrected = contrastCorrect(inputImage_resized, patchSize);
+    else
+        inputImage_corrected = contrastCorrect(inputImage_resized);
+    end
+end
+
+% Estimate the number of patches based on the given "patch density" (number
+% of patches per megapixel) and the non-zero pixels in the given label image
+
+numberOfPatches = round(sum(labelImage(:)>0)/((1024/downsamplingFactor)^2)*patchDensity);
+
+generatePatches(inputImage_corrected, labelImage, inputPatchDirectory, labelPatchDirectory, patchSize, numberOfPatches);
+
 end
 
 
