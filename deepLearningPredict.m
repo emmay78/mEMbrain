6945455e6699d800d80ad32fd357bcaa0e114479
Elisa Pavarino <epavarino@fas.harvard.edu>
2023-02-15 15:04:43 -0500

    original Emma repo + Elisa/Yaron mods

--- deepLearningPredict.m
+++ deepLearningPredict.m
@@ -4,8 +4,17 @@ function [predictions] = deepLearningPredict(inputImage,network)
 %   Uses semanticseg to make predictions based on a given trained network
 %   and input image
 
+% NOTE: the training of network has to have happened on multiple of 32 px *
+% 32 px. 
+size_image = size(inputImage);
+inputImage = padarray(inputImage,32*ceil(size_image/32)-size_image,'symmetric','post');
+
 [prediction, ~, probabilityMap] = semanticseg(inputImage, network, ...
     'OutputType', 'uint8');
+
+inputImage=inputImage(1:size_image(1),1:size_image(2));
+prediction=prediction(1:size_image(1),1:size_image(2));
+probabilityMap=probabilityMap(1:size_image(1),1:size_image(2),:);
 
 prediction = 255 - (double(prediction)-1)*255;
 probabilityMap = probabilityMap(:,:,1);
@@ -17,6 +26,5 @@ predictions.prediction = prediction;
 predictions.probability = probabilityMap;
 predictions.predictionWithInput = predictionWithInput;
 predictions.probabilityWithInput = probabilityWithInput;
-
 end
 
